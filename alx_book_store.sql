33686587c396ec4f13fb60e394ab0b3780d33da9
osugodbless <osugodbless234@gmail.com>
2025-06-30 00:55:38 +0100

    Designing MySQL database for an online book store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,15 @@
 -- Create the database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
+-- Use the database
+USE alx_book_store;
+
+-- Create the Authors table
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
 -- Create the Books table
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,12 +17,6 @@ CREATE TABLE Books (
     author_id INT,
     price DOUBLE,                                                   publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)       );
-
--- Create the Authors table
-CREATE TABLE Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
-);
 
 -- Create the Customers table
 CREATE TABLE Customers (
